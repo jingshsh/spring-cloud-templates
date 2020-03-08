7ea74a8bb59ee5c039a9542a21cc7055bc0a0455
HandsomJin <handsomjin@HandsomJindeMacBook-Pro.local>
2020-03-08 21:42:31 +0800

    feature#init spring cloud template repository

---
+++ spring-cloud-config/src/main/java/Application.java
@@ -0,0 +1,7 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+}
